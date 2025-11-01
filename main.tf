f8b67d06e76b648d25403e2539ae81a31892aea3
Sachin Jadhav <153444355+Sachinjadhav8@users.noreply.github.com>
2025-11-01 20:19:06 +0530

    testing main.tf with terraform cloud

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "jadhavsachinn01-org"  # your Terraform Cloud org name
+
+    workspaces {
+      name = "energyA"  # your workspace name
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "energya-terraform-demo-bucketxxx10"
+  tags = {
+    Name = "EnergyA Demo"
+  }
+}
