1857011ba67eb7901ef4aa49ba497e7a670f3a60
--replace-all <jadhavsachinn01@gmail.com>
2025-11-04 11:29:29 +0530

    updating vpc name

--- prod/modules/vpc/main.tf
+++ prod/modules/vpc/main.tf
@@ -1,7 +1,7 @@
 # -----------------------------
 # VPC
 # -----------------------------
-resource "aws_vpc" "this" {
+resource "aws_vpc" "thiss" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
