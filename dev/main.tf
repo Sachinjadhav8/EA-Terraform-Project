937329bc2b52fcf929a4ebc3d98b2a273290524c
--replace-all <jadhavsachinn01@gmail.com>
2025-11-08 22:54:18 +0530

    pushing code

--- dev/main.tf
+++ dev/main.tf
@@ -1,26 +1,3 @@
-terraform {
-  required_version = "~> 1.13.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-
-  # Option A: Terraform Cloud backend (uncomment and set values for your org/workspace)
-  cloud {
-    organization = "EA-ORG"   # <-- replace with your Terraform Cloud org name
-    workspaces {
-      name = "EA-Dev"  # <-- workspace name in Terraform Cloud
-    }
-  }
-}
-
-
-provider "aws" {
-  region = var.aws_region
-}
 
 module "iam_users" {
   source      = "./modules/iam-users"
