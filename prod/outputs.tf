5b44831cc2531bb5b8a741074c639941ac2d125b
--replace-all <jadhavsachinn01@gmail.com>
2025-11-08 22:28:40 +0530

    updating files

--- prod/outputs.tf
+++ prod/outputs.tf
@@ -71,13 +71,3 @@ output "private_asg_name" {
 
 */
 
-
-output "iam_users" {
-  description = "List of IAM users created"
-  value       = module.iam_users.user_names
-}
-
-output "iam_user_passwords_file" {
-  description = "Path to the local credentials file"
-  value       = module.iam_users.credentials_file
-}
